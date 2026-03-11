86a602d3a22c5fd25d33575d3581f8b711b3db0c
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-03-11 16:03:21 -0500

    Cree la primera parte del proyecto y subi el diagrama de flujo a la carpeta docs

---
+++ main.py
@@ -0,0 +1,53 @@
+print ("Sistema de inventario\n")
+
+#Entrada de datos y validacion de los datos
+nombre = input("Ingrese el nombre de su producto: ")
+print()
+
+while True:
+    if nombre.isalpha():
+        break
+        
+    elif nombre == "":    
+        print("Por favor ingresa el nombre del producto, no dejes el campo vacío")
+        print()
+        nombre = input("Ingrese el nombre de su producto: ")
+        
+    else: 
+        print("Entrada invalida, solo se permiten letras")
+        print()
+        nombre = input("Ingrese el nombre de su producto: ")
+        
+
+
+precio = float(input("Ingrese el precio de su producto $"))
+print()
+
+while precio <= 0:
+    print("Ingrese un precio valido, Intente nuavamente")
+    print()
+    precio = int(input("Ingrese el precio de su producto $"))
+    print()
+
+
+cantidad = int(input("Ingrese la cantidad de su producto: "))
+print()
+
+while cantidad <= 0:
+   print("Ingrese una cantidad mayor a 0, Intente nuavamente")
+   print()
+   cantidad = int(input("Ingrese la cantidad de su producto: "))
+   print()
+
+
+#Operacion matemática para sacar el total 
+
+costo_total = precio * cantidad
+
+#Mostras resultado en consola
+
+print("PRODUCTO =", nombre)
+print("PRECIO =", precio)
+print("CANTIDAD =", cantidad)
+print("TOTAL =", costo_total)
+
