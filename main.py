4ea0d5da1f9af56b44b9e6585a9370982e8248f5
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-03-12 15:36:20 -0500

    Mejore la validacion de los datos

--- main.py
+++ main.py
@@ -1,46 +1,60 @@
-print ("Sistema de inventario\n")
+print("Sistema de inventario\n")
 
-#Entrada de datos y validacion de los datos
+#Entrada de los datos del producto y validacion de los datos
 nombre = input("Ingrese el nombre de su producto: ")
 print()
 
 while True:
-    if nombre.isalpha():
+    if nombre.replace(" ","").isalpha():
         break
-        
-    elif nombre == "":    
+
+    elif nombre == "":
+        print()
         print("Por favor ingresa el nombre del producto, no dejes el campo vacío")
         print()
         nombre = input("Ingrese el nombre de su producto: ")
-        
-    else: 
+        print()
+
+    else:
+        print()
         print("Entrada invalida, solo se permiten letras")
         print()
         nombre = input("Ingrese el nombre de su producto: ")
-        
+        print()
 
 
-precio = float(input("Ingrese el precio de su producto $"))
-print()
+while True:
+    try:
+        precio = float(input("Ingrese el precio de su producto $"))
+        print()
 
-while precio <= 0:
-    print("Ingrese un precio valido, Intente nuavamente")
-    print()
-    precio = int(input("Ingrese el precio de su producto $"))
-    print()
+        if precio <= 0:
+            print("Ingrese un precio valido, Intente nuavamente")
+            print()
+            continue
+        break
+
+    except ValueError:
+        print("Error: Tienes que ingresar un valor númerico")
+        print()
+
+while True:
+    try:
+        cantidad = int(input("Ingrese la cantidad de su producto: "))
+        print()
+
+        if cantidad <= 0:
+            print("Ingrese una cantidad mayor a 0, Intente nuavamente")
+            print()
+            continue
+        break
+
+    except ValueError:
+        print("Error: Tienes que ingresar un valor númerico")
+        print()
 
 
-cantidad = int(input("Ingrese la cantidad de su producto: "))
-print()
-
-while cantidad <= 0:
-   print("Ingrese una cantidad mayor a 0, Intente nuavamente")
-   print()
-   cantidad = int(input("Ingrese la cantidad de su producto: "))
-   print()
-
-
-#Operacion matemática para sacar el total 
+#Operacion matemática para sacar el total
 
 costo_total = precio * cantidad
 
