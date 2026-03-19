c77050e5d70287d5e358954ec5b6682faf36642e
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-03-19 15:55:42 -0500

    Modificaciones visuales al mostrar el inventario  y organizacion del codigo del sistema

--- main.py
+++ main.py
@@ -1,3 +1,49 @@
+#Llamando funciones del sistema de inventario 
 
+from src.inventario import *
+
+# Sistema de inventario de productos
+print()
+print("Sistema de inventario".upper())
  
 
+#Program Main    
+while True:
+
+    print("-Menu-------------------------".upper())
+    print("| 1 → Agregar producto       |")
+    print("| 2 → Mostrar inventario     |")
+    print("| 3 → Calcular estadísticas  |")
+    print("| 4 → Salir                  |")
+    print("-"*30)
+
+    print()
+
+    accion = int(input("Escoge la opcion a realizar = "))
+
+    while accion not in (1, 2, 3, 4):
+
+        print()
+        print("Opción inválida")
+        print()
+
+        accion = int(input("Intente nuevamente = "))
+
+    print()
+
+    if accion == 1:       
+        agregar_producto()
+
+    elif accion == 2:
+        mostrar_inventario()
+              
+    elif accion == 3:
+        print("----Caculando estadisticas de inventario----\n".upper())
+
+        calcular_estadisticas() 
+
+        print("----Volviendo al menu----".upper())
+
+    else:
+        print("Saliendo del inventario\n".upper())
+        break
--- src/inventario.py
+++ src/inventario.py
@@ -1,11 +1,21 @@
-# Sistema de inventario de productos
-print("Sistema de inventario\n".upper())
-
-inventario = []
+inventario = [
+    {
+      "nombre": "Queso",
+      "precio": 200,
+      "cantidad": 3
+    },
+    {
+      "nombre": "Lulo",
+      "precio": 300,
+      "cantidad": 4
+    }
+]
 
 def agregar_producto():
     while True:
             while True:
+                print("-------Agregar producto-------\n".upper())
+
                 # Entrada de los datos del producto y validacion de los datos
                 nombre = input("Ingrese el nombre de su producto: ")
                 print()
@@ -16,7 +26,7 @@ def agregar_producto():
                 elif nombre == "":
                     print()
                     print(
-                        "Por favor ingresa el nombre del producto, no dejes el campo vacío"
+                        "No dejes el campo vacío, por favor ingresa el nombre del producto"
                     )
                     continue
 
@@ -31,7 +41,7 @@ def agregar_producto():
                     print()
 
                     if precio <= 0:
-                        print("Ingrese un precio valido, Intente nuavamente")
+                        print("Ingrese un precio valido, Intente nuevamente")
                         print()
                         continue
                     break
@@ -47,7 +57,7 @@ def agregar_producto():
                     print()
 
                     if cantidad <= 0:
-                        print("Ingrese una cantidad mayor a 0, Intente nuavamente")
+                        print("Ingrese una cantidad mayor a 0, Intente nuevamente")
                         print()
                         continue
                     break
@@ -66,21 +76,40 @@ def agregar_producto():
                 
             print("Producto agregado correctamente.\n")
            
-            continuar = input("¿Quiere agregar otro producto? (Si/No): ".lower())
+            continuar = input("¿Quiere agregar otro producto? (Si/No): ").lower()
 
             print()
         
-            if continuar != "si":
-                print("----Volviendo al menu----".upper())
+            if continuar not in ("si","s","yes"):
+                print("------Volviendo al menu-------\n".upper())
                 break  
 
 def mostrar_inventario():
+    print("-----Mostrando inventario-----\n".upper())
+    
+    # if not inventario:
+    #         print ("Inventario Vacio\n")
+
+    # else:
+    #     for p in inventario:
+    #         print(f"| Producto: {p['nombre'].upper()} | Precio: ${p['precio']} | Cantidad: {p['cantidad']} |")
+    #         print("-"*50)
+    #         print()
+    
+    
+    print("+---------------+----------+-----------+")
+    print("| PRODUCTO      | PRECIO   | CANTIDAD  |")
+    print("+---------------+----------+-----------+")
     if not inventario:
             print ("Inventario Vacio\n")
 
     else:
         for p in inventario:
-            print(f"Producto: {p['nombre'].upper()} | Precio: ${p['precio']} | Cantidad: {p['cantidad']}\n")
+            print(f"| {p['nombre']:<13} | ${p['precio']:<7} | {p['cantidad']:<9} |")
+
+    print("+---------------+----------+-----------+\n")
+    print("------Volviendo al menu-------\n".upper())
+
 
 def calcular_estadisticas():
     total_productos = len(inventario)
@@ -97,49 +126,3 @@ def calcular_estadisticas():
     print(f"La cantidad total de productos registrados es: {total_unidades}\n")
     print(f"El valor total del inventario es: ${total_precio}\n")
     
-#Program Main    
-while True:
-
-    print()
-
-    print("1 → Agregar producto")
-    print("2 → Mostrar inventario")
-    print("3 → Calcular estadísticas")
-    print("4 → Salir")
-
-    print()
-
-    accion = int(input("Escoge la opcion a realizar = "))
-
-    while accion not in (1, 2, 3, 4):
-
-        print()
-        print("Opción inválida")
-        print()
-
-        accion = int(input("Intenta nuevamente = "))
-
-    print()
-
-    if accion == 1:
-        print("----Agregar producto----\n".upper())
-        
-        agregar_producto()
-
-    elif accion == 2:
-        print("----Mostrando inventario----\n".upper())
-
-        mostrar_inventario()
-        
-        print("----Volviendo al menu----".upper())
-
-    elif accion == 3:
-        print("----Caculando estadisticas de inventario----\n".upper())
-
-        calcular_estadisticas() 
-
-        print("----Volviendo al menu----".upper())
-
-    else:
-        print("Saliendo del inventario".upper())
-        break
