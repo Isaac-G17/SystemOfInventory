cb8855bf3480201af6c6e95ff18f693b9e07514b
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-03-24 15:45:45 -0500

    Nuevas opciones del menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Llamando funciones del sistema de inventario 
+# Llamando funciones del sistema de inventario
 
 from src.inventario import *
 
@@ -7,43 +7,62 @@ print()
 print("Sistema de inventario".upper())
 
 
-#Program Main    
+# Program Main
 while True:
 
-    print("-Menu-------------------------".upper())
-    print("| 1 → Agregar producto       |")
-    print("| 2 → Mostrar inventario     |")
-    print("| 3 → Calcular estadísticas  |")
-    print("| 4 → Salir                  |")
-    print("-"*30)
+    print("-Menu-----------------------------".upper())
+    print("| 1 → Agregar producto           |")
+    print("| 2 → Mostrar inventario         |")
+    print("| 3 → Buscar producto            |")
+    print("| 4 → Actualizar producto        |")
+    print("| 5 → Eliminar producto          |")
+    print("| 6 → Estadísticas de inventario |")
+    print("| 7 → Guardar CSV                |")
+    print("| 8 → Cargar CSV                 |")
+    print("| 9 → Salir                      |")
+    print("-" * 34)
 
     print()
 
     try:
         accion = int(input("Escoge la opcion a realizar = "))
-        
-        if accion not in (1, 2, 3, 4):
+
+        if accion not in (1, 2, 3, 4, 5, 6, 7, 8, 9):
             print()
-            print("Opción inválida")
+            print("Error: Opción inválida")
             print()
             accion = int(input("Intente nuevamente = "))
 
     except ValueError:
         print("Error: Tienes que ingresar un valor númerico\n")
         continue
-    
+
     print()
 
-    if accion == 1:       
+    if accion == 1:
         agregar_producto()
 
     elif accion == 2:
         mostrar_inventario()
-        
+
     elif accion == 3:
-        calcular_estadisticas() 
+        pass
+
+    elif accion == 4:
+        pass
+
+    elif accion == 5:
+        pass
+
+    elif accion == 6:
+        calcular_estadisticas(inventario)
+
+    elif accion == 7:
+        pass
+
+    elif accion == 8:
+        pass
 
     else:
         print("Saliendo del inventario\n".upper())
         break
-        
--- src/inventario.py
+++ src/inventario.py
@@ -1,95 +1,78 @@
 inventario = [
-    {
-      "nombre": "Queso",
-      "precio": 2000000,
-      "cantidad": 3
-    },
-    {
-      "nombre": "Lulo",
-      "precio": 300,
-      "cantidad": 4
-    }
+    {"nombre": "Queso", "precio": 200.0, "cantidad": 3},
+    {"nombre": "Lulo", "precio": 300.0, "cantidad": 4},
+    {"nombre": "Arroz", "precio": 400.0, "cantidad": 4},
 ]
 
-def agregar_producto():
-    while True:
-            while True:
-                print("-------Agregar producto-------\n".upper())
 
-                # Entrada de los datos del producto y validacion de los datos
-                nombre = input("Ingrese el nombre de su producto: ")
+def agregar_producto():
+    print("-------Agregar producto-------\n".upper())
+    while True:
+        while True:
+            # Entrada de los datos del producto y validacion de los datos
+            nombre = input("Ingrese el nombre de su producto: ")
+            print()
+
+            if nombre.replace(" ", "").isalpha():
+                break
+
+            elif nombre == "":
+                print(
+                    "No dejes el campo vacío, por favor ingresa el nombre del producto\n"
+                )
+                continue
+
+            else:
+                print("Entrada invalida, solo se permiten letras\n")
+                continue
+
+        while True:
+            try:
+                precio = float(input("Ingrese el precio de su producto $"))
                 print()
 
-                if nombre.replace(" ", "").isalpha():
-                    break
-
-                elif nombre == "":
-                    print()
-                    print(
-                        "No dejes el campo vacío, por favor ingresa el nombre del producto"
-                    )
+                if precio <= 0:
+                    print("Error: Ingrese un precio valido, Intente nuevamente\n")
                     continue
+                break
+            except ValueError:
+                print()
+                print("Error: Tienes que ingresar un valor númerico\n")
 
-                else:
-                    print()
-                    print("Entrada invalida, solo se permiten letras")
+        while True:
+            try:
+                cantidad = int(input("Ingrese la cantidad de su producto: "))
+                print()
+
+                if cantidad <= 0:
+                    print("Error: Ingrese una cantidad mayor a 0, Intente nuevamente\n")
                     continue
+                break
+            except ValueError:
+                print()
+                print("Error: Tienes que ingresar un valor númerico\n")
 
-            while True:
-                try:
-                    precio = float(input("Ingrese el precio de su producto $"))
-                    print()
+        producto = {"nombre": nombre, "precio": precio, "cantidad": cantidad}
 
-                    if precio <= 0:
-                        print("Ingrese un precio valido, Intente nuevamente")
-                        print()
-                        continue
-                    break
-                    
+        inventario.append(producto)
 
-                except ValueError:
-                    print("Error: Tienes que ingresar un valor númerico")
-                    print()
+        print("Producto agregado correctamente.\n")
+        continuar = input("¿Quiere agregar otro producto? (Si/No): ").lower()
 
-            while True:
-                try:
-                    cantidad = int(input("Ingrese la cantidad de su producto: "))
-                    print()
+        print()
 
-                    if cantidad <= 0:
-                        print("Ingrese una cantidad mayor a 0, Intente nuevamente")
-                        print()
-                        continue
-                    break
-                except ValueError:
-                    print("Error: Tienes que ingresar un valor númerico")
-                    print()
-            
-            producto = {
-                "nombre": nombre,
-                "precio": precio,
-                "cantidad": cantidad
-            }
-
-            inventario.append(producto)
-                
-            print("Producto agregado correctamente.\n")
-            continuar = input("¿Quiere agregar otro producto? (Si/No): ").lower()
-
-            print()
-        
-            if continuar not in ("si","s","yes"):
-                print("------Volviendo al menu-------\n".upper())
-            break  
+        if continuar not in ("si", "s", "yes"):
+            print("------Volviendo al menu-------\n".upper())
+        break
 
 def mostrar_inventario():
     print("-----Mostrando inventario-----\n".upper())
-        
+
     print("+---------------+-------------+-----------+")
     print("| PRODUCTO      | PRECIO      | CANTIDAD  |")
     print("+---------------+-------------+-----------+")
     if not inventario:
-            print ("Inventario Vacio\n")
+        print("Inventario Vacio\n")
 
     else:
         for p in inventario:
@@ -99,21 +82,26 @@ def mostrar_inventario():
     print("------Volviendo al menu-------\n".upper())
 
 
-def calcular_estadisticas():
+def calcular_estadisticas(inventario):
     print("----Caculando estadisticas de inventario----\n".upper())
 
     total_productos = len(inventario)
-    total_precio = 0
-    total_unidades = 0
+    valor_total = 0
+    unidades_totales = 0
 
     # Operacion matemática para tener las estadisticas de inventario
     for p in inventario:
-        total_precio += p['precio'] * p['cantidad']
-        total_unidades += p['cantidad']
+        valor_total += p["precio"] * p["cantidad"]
+        unidades_totales += p["cantidad"]
 
+    producto_max = max(inventario, key=lambda p: p["precio"])
+    max_cantidad = max(inventario, key=lambda p: p["cantidad"])
 
-    print(f"El total de productos es: {total_productos}\n")
-    print(f"La cantidad total de productos registrados es: {total_unidades}\n")
-    print(f"El valor total del inventario es: ${total_precio}\n")
-
+    print(f"Productos en total: {total_productos}\n")
+    print(f"Unidades totales: {unidades_totales}\n")
+    print(f"Valor total: ${valor_total}\n")
+    print(f"Producto más caro: {producto_max['nombre']} | {producto_max['precio']}\n")
+    print(
+        f"Producto con mayor stock: {max_cantidad['nombre']} | {max_cantidad['cantidad']}\n"
+    )
     print("-------------Volviendo al menú--------------\n".upper())
