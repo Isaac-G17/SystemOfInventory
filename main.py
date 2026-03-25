d97e69c0226034fdea8d905e3aa67bd3eb89b3e0
Isaac Guzman <guzmanisaac2005@outlook.com>
2026-03-25 09:14:06 -0500

    Termine el codigo de las opciones de buscar,actualizar y eliminar producto

--- main.py
+++ main.py
@@ -1,68 +1,9 @@
 # Llamando funciones del sistema de inventario
-
-from src.inventario import *
+from src.servicios import *
+from src.app import *
 
 # Sistema de inventario de productos
 print()
 print("Sistema de inventario".upper())
 
-
-# Program Main
-while True:
-
-    print("-Menu-----------------------------".upper())
-    print("| 1 → Agregar producto           |")
-    print("| 2 → Mostrar inventario         |")
-    print("| 3 → Buscar producto            |")
-    print("| 4 → Actualizar producto        |")
-    print("| 5 → Eliminar producto          |")
-    print("| 6 → Estadísticas de inventario |")
-    print("| 7 → Guardar CSV                |")
-    print("| 8 → Cargar CSV                 |")
-    print("| 9 → Salir                      |")
-    print("-" * 34)
-
-    print()
-
-    try:
-        accion = int(input("Escoge la opcion a realizar = "))
-
-        if accion not in (1, 2, 3, 4, 5, 6, 7, 8, 9):
-            print()
-            print("Error: Opción inválida")
-            print()
-            accion = int(input("Intente nuevamente = "))
-
-    except ValueError:
-        print("Error: Tienes que ingresar un valor númerico\n")
-        continue
-
-    print()
-
-    if accion == 1:
-        agregar_producto()
-
-    elif accion == 2:
-        mostrar_inventario()
-
-    elif accion == 3:
-        pass
-
-    elif accion == 4:
-        pass
-
-    elif accion == 5:
-        pass
-
-    elif accion == 6:
-        calcular_estadisticas(inventario)
-
-    elif accion == 7:
-        pass
-
-    elif accion == 8:
-        pass
-
-    else:
-        print("Saliendo del inventario\n".upper())
-        break
+menu()
---
+++ src/app.py
@@ -0,0 +1,156 @@
+from src.servicios import *
+
+def pedir_nombre():
+    """
+    Solicita un nombre válido (solo letras y espacios).
+    """
+    nombre = ""
+
+    while nombre.strip() == "" or not nombre.replace(" ", "").isalpha():
+        nombre = input("Ingrese el nombre de su producto:")
+
+        if nombre.strip() == "":
+            print("No dejes el campo vacío\n")
+        elif not nombre.replace(" ", "").isalpha():
+            print("Solo se permiten letras\n")
+
+    return nombre
+
+def pedir_float(mensaje):
+    valor = -1
+
+    while valor < 0:
+        try:
+            valor = float(input(mensaje))
+
+            if valor < 0:
+                print("Debe ser mayor o igual a 0\n")
+
+        except ValueError:
+            print("Entrada inválida\n")
+            valor = -1  
+    return valor
+
+def pedir_int(mensaje):
+    """
+    Solicita un número entero válido al usuario.
+
+    Parámetros:
+        mensaje (str): Texto a mostrar.
+
+    Retorna:
+        int: Valor ingresado válido.
+    """
+    valor = -1
+
+    while valor < 0:
+        try:
+            valor = int(input(mensaje))
+
+            if valor < 0:
+                print("Debe ser mayor o igual a 0\n")
+
+        except ValueError:
+            print("Entrada inválida\n")
+            valor = -1  
+
+    return valor
+
+def menu():
+    """
+    Muestra el menú principal y controla el flujo del programa.
+
+    Retorna:
+        None
+    """
+    accion = 0
+
+    while accion != 9:
+
+        print("-Menu-----------------------------".upper())
+        print("| 1 → Agregar producto           |")
+        print("| 2 → Mostrar inventario         |")
+        print("| 3 → Buscar producto            |")
+        print("| 4 → Actualizar producto        |")
+        print("| 5 → Eliminar producto          |")
+        print("| 6 → Estadísticas de inventario |")
+        print("| 7 → Guardar CSV                |")
+        print("| 8 → Cargar CSV                 |")
+        print("| 9 → Salir                      |")
+        print("-" * 34)
+
+        try:
+            accion = int(input("Seleccione una opción: "))
+            print()
+        except ValueError:
+            print("Opción inválida\n")
+            continue
+
+        if accion == 1:
+            print("-------Agregar producto-------\n".upper())
+            continuar = "si"
+            while continuar in ("si", "s", "yes"):
+                nombre = pedir_nombre()
+                print()
+                precio = pedir_float("Ingrese el precio de su producto $")
+                print()
+                cantidad = pedir_int("Ingrese la cantidad de su producto: ")
+                print()
+                agregar_producto(inventario, nombre, precio, cantidad)
+                print(f"Producto agregado correctamente\n")
+
+                continuar = input("¿Quiere agregar otro producto? (Si/No): ").lower()
+
+                print()
+
+                if continuar not in ("si", "s", "yes"):
+                    print("------Volviendo al menu-------\n".upper())
+            
+        elif accion == 2:
+            mostrar_inventario(inventario)
+
+        elif accion == 3:
+            nombre = input("Buscar producto: ")
+            producto = buscar_producto(inventario, nombre)
+
+            if producto:
+                print(producto, "\n")
+            else:
+                print("Producto no encontrado\n")
+
+        elif accion == 4:
+            nombre = input("Producto a actualizar: ")
+
+            nuevo_precio = input("Nuevo precio (enter para omitir): ")
+            nueva_cantidad = input("Nueva cantidad (enter para omitir): ")
+
+            nuevo_precio = float(nuevo_precio) if nuevo_precio else None
+            nueva_cantidad = int(nueva_cantidad) if nueva_cantidad else None
+
+            actualizar_producto(inventario, nombre, nuevo_precio, nueva_cantidad)
+
+        elif accion == 5:
+            nombre = input("Producto a eliminar: ")
+            eliminar_producto(inventario, nombre)
+
+        elif accion == 6:
+            print("----Estadisticas de inventario----\n".upper())
+
+            stats = calcular_estadisticas(inventario)
+
+            if stats:
+                print(f"Total productos: {stats['productos_registrados']}\n")
+                print(f"Unidades totales: {stats['unidades_totales']}\n")
+                print(f"Valor total: ${stats['valor_total']:.2f}\n")
+                print(f"Producto más caro: {stats['producto_mas_caro']['nombre']} | ${stats['producto_mas_caro']['precio']:.2f}\n")
+                print(f"Mayor stock: {stats['producto_mayor_stock']['nombre']} | {stats['producto_mayor_stock']['cantidad']:.2f}\n")
+            else:
+                print("Inventario vacío\n")
+
+            print("------Volviendo al menu-------\n".upper())
+
+        elif accion == 9:
+            print("Saliendo del inventario\n".upper())
+        else:
+            print("Error: Opción inválida\n")
+
